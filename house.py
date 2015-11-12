80d1f5c7e35f51658a6a2ab6e129f06d8e93a1b5
Mike Franklin <mfranklin@mgh.harvard.edu>
2015-11-12 18:43:17 -0500

    set up templates+ includes; adjust load date to be date of redfin file

--- house.py
+++ house.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask.ext.pymongo import PyMongo
 import os
 import csv
@@ -14,49 +14,56 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route("/listings")
+def show_listings():
+    """ show listings """
+
+    return render_template("index.html")
+
+
 @app.route('/import')
 def import_redfin():
     """
     load & save the latest redfin CSV
     """
 
-    listings = load_latest_listings()
-    res = save_listings(listings)
+    listings, filedate = load_latest_listings()
+    res = save_listings(listings, filedate)
     return json.dumps(res)
 
 
-def save_listings(listings):
+def save_listings(listings, date):
     """ update house info """
 
     bulk = mongo.db.listings.initialize_unordered_bulk_op()
-    now = datetime.now()
 
     for listing in listings:
         if "listing id" in listing:
             listing["_id"] = listing.pop("listing id")
-            listing["last_loaded"] = now
+            listing["last_loaded"] = date
             bulk.find({"_id": listing["_id"]}).upsert().update({"$set": listing})
 
     return bulk.execute()
 
 
 def load_latest_listings():
-    " find & import the latest redfin csv file as a dict "
+    " find & import the latest redfin csv file as a dict & date "
+    # redfin_2015-11-12-14-27-48_results
 
     dir = "/Users/michaelfranklin/Downloads"
     files = [file for file in os.listdir(dir)
              if file.startswith("redfin") and file.endswith("results.csv")]
+    file = sorted(files)[-1]
+    date = datetime.strptime(file[7:-12], "%Y-%m-%d-%H-%M-%S")
 
-    file = "{}/{}".format(dir, sorted(files)[-1])
-
-    with open(file) as csvfile:
+    with open("{}/{}".format(dir, file)) as csvfile:
         reader = csv.reader(csvfile)
         raw = next(reader)
         titles = ["url" if title.startswith("URL ") else title.lower()
                   for title in raw]
         data = [dict(zip(titles, row)) for row in reader]
 
-    return data
+    return (data, date)
 
 
 if __name__ == '__main__':
